1121970c6b95f7e041bc7945ec42c2e5574e8366
dflesh <dflesh1111@yandex.ru>
2019-12-05 17:00:32 +0300

    почистил

--- PopularWords.py
+++ PopularWords.py
@@ -9,17 +9,15 @@ import re
 def search_words():
     texts = db.select_posts_text()
     stopwords_ru = nltk.corpus.stopwords.words('russian')
-    #print(stopwords_ru)
     morph = pymorphy2.MorphAnalyzer()
     words = []
-    other_symbols = ('«', '»', '``')
     for text in texts:
         tokenizer = nltk.tokenize.TreebankWordTokenizer()
         tokens = tokenizer.tokenize(text)
         text = " ".join(morph.normal_forms(token)[0] for token in tokens)
         tokens = tokenizer.tokenize(text)
         for token in tokens:
-            if token in stopwords_ru or token in string.punctuation or token in other_symbols:
+            if token in stopwords_ru or token in string.punctuation:
                 continue
             else:
                 if re.search(r'[^а-яА-Я]', token):
@@ -27,8 +25,8 @@ def search_words():
                 else:
                     words.append(token)
     words_popularity = Counter(words)
-    print(words_popularity)
-    print(*words_popularity.most_common(10), sep="\n")
+    # print(words_popularity)
+    # print(*words_popularity.most_common(10), sep="\n")
     return words_popularity
 
 
